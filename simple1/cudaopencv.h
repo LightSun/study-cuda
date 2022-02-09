ecb6d39a04910d4de827c02d3392463e129c7a6f
heaven7 <532278976@qq.com>
2022-02-09 12:50:25 +0800

    opt

--- simple1/cudaopencv.h
+++ simple1/cudaopencv.h
@@ -5,7 +5,17 @@
 #include <opencv2/opencv.hpp>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+/**
+ //cuda::cvtColor(gpuMat, grayGpuMat, cuda::COLOR_RGB2GRAY);
+ //cuda::resize
+ //cv::cuda::Stream stream;
+ //cv::cuda::threshold(m, m, 128, 255, cv::THRESH_BINARY, stream);
 
+  //cuda convert to.
+  cv::cuda::GpuMat m(cv::Size(1280, 1024), CV_32FC1);
+  cv::cuda::Stream stream;
+  m.convertTo(m, CV_8UC1, 1, 0, stream);
+ */
 class CudaOpencv
 {
 public:
